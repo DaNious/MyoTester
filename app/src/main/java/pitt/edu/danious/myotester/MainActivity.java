8781c8581be62bd53299698c7af827858a953238
Xing-zhe Song <xz.song1993@gmail.com>
2019-09-01 12:36:41 -0400

    Welcome Page Done

--- app/src/main/java/pitt/edu/danious/myotester/MainActivity.java
+++ app/src/main/java/pitt/edu/danious/myotester/MainActivity.java
@@ -2,7 +2,9 @@ package pitt.edu.danious.myotester;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //called when user proceed to next
+    public void start2Test(View view){
+        Intent intent = new Intent(this, WorkoutActivity.class);
+        startActivity(intent);
     }
 }
