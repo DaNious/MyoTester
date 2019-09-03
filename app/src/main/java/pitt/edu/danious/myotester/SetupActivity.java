89fea9dc625f9f38f33828bb9f2682ec64840d48
Xing-zhe Song <xz.song1993@gmail.com>
2019-09-02 22:15:36 -0400

    add setup page

--- app/src/main/java/pitt/edu/danious/myotester/SetupActivity.java
+++ app/src/main/java/pitt/edu/danious/myotester/SetupActivity.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SetupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setup);
     }
 
-    //called when user proceed to next
-    public void start2Setup(View view){
-        Intent intent = new Intent(this, SetupActivity.class);
+    //called when user confirm the information
+    public void setup2Workout(View view){
+        Intent intent = new Intent(this, WorkoutActivity.class);
         startActivity(intent);
     }
 }
